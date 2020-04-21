4f79fd24276a597b5a6bb0f59b6463e44aeb436d
Wael Ibrahim <ibrahim.88wael@yahoo.de>
2020-04-21 11:02:55 +0200

    21-04-2020

---
+++ index.js
@@ -0,0 +1,105 @@
+// Select cvs
+const cvs = document.getElementById('bird')
+const ctx = cvs.getContext('2d')
+
+// Game Vars And Consts
+let frames = 0
+
+// Load Sprite Image
+const sprite = new Image();
+sprite.src = "img/sprite.png";
+
+
+// BACKGROUND
+const bg = {
+    sX : 0,
+    sY : 0,
+    w : 275,
+    h : 226,
+    x : 0,
+    y : cvs.height - 226,
+
+    draw : function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h )
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h )
+    }
+}
+
+const fg = {
+    sX: 276,
+    sY: 0,
+    w: 224,
+    h: 112,
+    x: 0,
+    y: cvs.height -112,
+    draw : function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h )
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h )
+    }
+
+}
+
+// BIRD 
+const bird = {
+    animation : [
+        {sX: 276, sY : 112},
+        {sX: 276, sY : 139 },
+        {sX: 276, sY : 164},
+        {sX: 276, sY : 139}
+    ],
+    x : 50,
+    y : 150,
+    w : 34,
+    h : 26,
+
+    frame : 0,
+
+    draw : function(){
+        let bird = this.animation[this.frame];
+
+        ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, this.x - this.w/2, this.y - this.h/2, this.w, this.h )
+        
+    }
+
+}
+
+// Get Ready Message
+const getReady = {
+    sX: 0,
+    sY: 228,
+    w: 173,
+    h: 152,
+    x: cvs.width/2 - 173/2,
+    y: 80,
+
+    draw : function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h )
+    }
+}
+
+// DRAW
+function draw() {
+    ctx.fillStyle = "#70c5ce";
+    ctx.fillRect(0, 0, cvs.width, cvs.height )
+
+    bg.draw();
+    fg.draw()
+    bird.draw()
+    getReady.draw()
+
+}
+ // UPDATE
+function update(){
+
+
+}
+
+// LOOP
+function loop() {
+    update()
+    draw()
+    frames++
+    requestAnimationFrame(loop)
+}
+
+loop()
